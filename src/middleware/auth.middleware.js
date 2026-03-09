c1d17ae01ece950687dd6ffd999cb5bfd9e34938
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-03-10 03:33:34 +0530

    Logout api implemented

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
-
+const tokenBlackListModel = require("../models/blacklist.model")
 
 async function authMiddleware(req,res,next){
     const token = req.cookies.token || req.headers.authorization?.split("")[1]
@@ -8,6 +8,14 @@ async function authMiddleware(req,res,next){
     if(!token){
         return res.status(401).json({
             message: "Unauthorized access, token is missing."
+        })
+    }
+
+    const isBlacklisted = await tokenBlackListModel.findOne({token})
+
+    if(isBlacklisted){
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
         })
     }
 
@@ -35,6 +43,14 @@ async function authSystemUserMiddleware(req,res,next){
         })
     }
 
+    const isBlacklisted = await tokenBlackListModel.findOne({token})
+
+    if(isBlacklisted){
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+
     try{
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
 
@@ -55,6 +71,7 @@ async function authSystemUserMiddleware(req,res,next){
         })
     }
 }
+
 
 module.exports = {
     authMiddleware,
