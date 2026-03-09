c9f7b7bd983fe348b945d2849c434d4651c4f23e
Jagriti Kanwar <kanwarjagritijee21@gmail.com>
2026-03-10 03:50:53 +0530

    testing api implemented

--- src/app.js
+++ src/app.js
@@ -19,6 +19,9 @@ const transactionRoutes = require("./routes/transaction.routes")
 * - Use Routes
 */
 
+app.get("/",(req,res)=>{
+    res.send("Ledger service is up and running.")
+})
 app.use("/api/auth", authRouter)
 app.use("/api/accounts",accountRouter)
 app.use("/api/transactions",transactionRoutes)
